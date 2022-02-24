18f6416060055684c2cd1a7aef43d793f08951e3
kingkej <63109924+kingkej@users.noreply.github.com>
2022-02-24 16:24:54 +0300

    Bug fixes

--- Sources/ExtraLottie/ExtraLottie.swift
+++ Sources/ExtraLottie/ExtraLottie.swift
@@ -3,17 +3,36 @@ import Lottie
 import SwiftUI
 
 public struct ExtraLottieView: UIViewRepresentable {
-    var name = "success"
-    var loopMode: LottieLoopMode = .loop
-    var repeats = false
-    var delayTime: Double = 0.0
-    let animationView = AnimationView()
+    var animationName: String
+    var loopMode: LottieLoopMode
+    var manualLoop = false
+    var delayBetweenLoop: Double = 0.0
     var fromProgress: CGFloat = 0.0
+    
+    let animationView = AnimationView()
+    
+    public init(animationName: String) {
+        self.animationName = animationName
+        self.loopMode = .loop
+    }
+    
+    public init(animationName: String, loopMode: LottieLoopMode) {
+        self.animationName = animationName
+        self.loopMode = loopMode
+        self.manualLoop = false
+    }
+    
+    public init(animationName: String, manualLoop: Bool, delayBetweenLoop: Double, fromProgress: CGFloat) {
+        self.animationName = animationName
+        self.manualLoop = manualLoop
+        self.fromProgress = fromProgress
+        self.loopMode = .playOnce
+    }
     
     public func makeUIView(context: UIViewRepresentableContext<ExtraLottieView>) -> UIView {
         let view = UIView(frame: .zero)
 
-        let animation = Animation.named(name)
+        let animation = Animation.named(animationName)
         animationView.animation = animation
         animationView.contentMode = .scaleAspectFit
         animationView.loopMode = loopMode
@@ -35,8 +54,8 @@ public struct ExtraLottieView: UIViewRepresentable {
     
     func playAnimation() {
         animationView.play(fromProgress: fromProgress, toProgress: 1) { _ in
-            if repeats {
-                DispatchQueue.main.asyncAfter(deadline: .now() + delayTime) {
+            if manualLoop {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delayBetweenLoop) {
                     playAnimation()
                 }
             }
